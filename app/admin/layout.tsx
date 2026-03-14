abf11635c5274123665a2f620071a16d23963fd5
arielbatoon09 <info.arielbatoon@gmail.com>
2026-03-14 18:57:12 +0800

    refactor: Remove unused admin layout and page components to streamline the admin section.

--- app/admin/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <main className="bg-green-100 h-screen">
-      { children }
-    </main>
-  );
-}
