6e28cc3e59c20cfe556524ab0423a95aa37c1a95
arielbatoon09 <info.arielbatoon@gmail.com>
2026-03-28 11:18:25 +0800

    Updated the homepage title
    - test1
    - tes2

--- components/features/home/Hero.tsx
+++ components/features/home/Hero.tsx
@@ -10,8 +10,8 @@ export function Hero() {
             <div className="flex flex-col items-center lg:items-start justify-center space-y-8">
                 <div className="space-y-4">
                     <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-foreground to-foreground/70">
-                        Building Exceptional <br className="hidden md:inline" />
-                        Digital Experiences
+                        Building Software <br className="hidden md:inline" />
+                        Digital Solutions
                     </h1>
                     <p className="max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                         I&apos;m a Full Stack Developer specializing in building (and occasionally designing) simple, beautiful, and intuitive interfaces.
